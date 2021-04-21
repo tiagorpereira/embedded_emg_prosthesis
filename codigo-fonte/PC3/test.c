9c7bfffc9536bdeec4d25172dd530a9c1298d222
Gabriel Carmona <gabrielgcarmona@gmail.com>
2021-04-21 12:41:02 -0300

    Teste mqtt

---
+++ codigo-fonte/PC3/test.c
@@ -0,0 +1,113 @@
+/*
+* Includes
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+//#include <sys/time.h>
+#include <MQTTClient.h>
+#include <time.h>
+
+/*
+* Defines
+*/
+/* Caso desejar utilizar outro broker MQTT, substitua o endereco abaixo */
+#define MQTT_ADDRESS   "tcp://127.0.0.1:1883"
+/* Substitua este por um ID unico em sua aplicacao */
+#define CLIENTID       "test"  
+
+/* Substitua aqui os topicos de publish e subscribe por topicos exclusivos de sua aplicacao */
+#define MQTT_PUBLISH_TOPIC     "MQTTCClientPubTopic"
+#define MQTT_SUBSCRIBE_TOPIC   "dados"
+
+/*
+*  Variaveis globais
+*/
+MQTTClient client;
+int t=0;
+int ant=0;
+/*
+* Prototipos de funcao
+*/
+void publish(MQTTClient client, char* topic, char* payload);
+int on_message(void *context, char *topicName, int topicLen, MQTTClient_message *message);
+
+/*
+* Implementacoes
+*/
+
+/* Funcao: publicacao de mensagens MQTT
+ * Parametros: cleinte MQTT, topico MQTT and payload
+ * Retorno: nenhum
+*/
+void publish(MQTTClient client, char* topic, char* payload) {
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+
+    pubmsg.payload = payload;
+    pubmsg.payloadlen = strlen(pubmsg.payload);
+    pubmsg.qos = 2;
+    pubmsg.retained = 0;
+    MQTTClient_deliveryToken token;
+    MQTTClient_publishMessage(client, topic, &pubmsg, &token);
+    MQTTClient_waitForCompletion(client, token, 1000L);
+}
+
+/* Funcao: callback de mensagens MQTT recebidas e echo para o broker
+ * Parametros: contexto, ponteiro para nome do topico da mensagem recebida, tamanho do nome do topico e mensagem recebida
+ * Retorno : 1: sucesso (fixo / nao ha checagem de erro neste exemplo)
+*/
+int n=0;
+int on_message(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
+    /*struct timeval timev;
+    gettimeofday(&timev, NULL);
+    t=(double)timev.tv_sec+((double)timev.tv_usec)*0.000001;
+    printf("Freq: %.2fHz\n",1/(t-ant));*/
+    n++;
+    t=time(NULL);
+    if(t-ant == 1){
+        printf("Freq: %dHz\n",n);
+        n=0;
+    }
+    ant=t;
+    char* payload = message->payload;
+
+    /* Mostra a mensagem recebida */
+    //printf("Mensagem recebida! \n\rTopico: %s Mensagem: %s\n", topicName, payload);
+
+    /* Faz echo da mensagem recebida */
+    //publish(client, MQTT_PUBLISH_TOPIC, payload);
+
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+    ant=t;
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
+   int rc;
+   MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+
+   /* Inicializacao do MQTT (conexao & subscribe) */
+   MQTTClient_create(&client, MQTT_ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+   MQTTClient_setCallbacks(client, NULL, NULL, on_message, NULL);
+
+   rc = MQTTClient_connect(client, &conn_opts);
+
+   if (rc != MQTTCLIENT_SUCCESS)
+   {
+       printf("\n\rFalha na conexao ao broker MQTT. Erro: %d\n", rc);
+       exit(-1);
+   }
+
+   MQTTClient_subscribe(client, MQTT_SUBSCRIBE_TOPIC, 0);
+
+   while(1)
+   {
+       /*
+        * o exemplo opera por "interrupcao" no callback de recepcao de 
+        * mensagens MQTT. Portanto, neste laco principal eh preciso fazer
+        * nada.
+        */
+   }
+}
